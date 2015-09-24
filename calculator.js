acf0105ff6ae9c306385304da026bae79d1faba7
Maxime Pare <maxime.pare.4@ulaval.ca>
2015-09-24 18:17:22 -0400

    Manque le point ( pas obligatoire) sin,cos,tan, geolocalisation, exposant (pas obligatoire), factoriel ne fonctionne pas parfaitement

--- calculator.js
+++ calculator.js
@@ -12,7 +12,7 @@ Math.factorial = function(n) {
         return f[n];
     }
     return f[n] = Math.factorial(n-1) * n;
-}
+};
 
 // Classe Calculator
 // Toutes les méthodes sauf `equals` retournent `this`, ce qui permet de chainer les appels
@@ -33,13 +33,13 @@ var Calculator = function () {
             equation += parseFloat(value);
         }
         return this;
-    }
+    };
 
     // Réinitialiser l'équation
     this.clear = function() {
         equation = '';
         return this;
-    }
+    };
 
     this.add = function(value) {
         equation += '+';
@@ -47,26 +47,26 @@ var Calculator = function () {
             equation += parseFloat(value);
         }
         return this;
-    }
+    };
 
     this.subtract = function (value) {
-        equation += '-'
+        equation += '-';
         if(typeof value !== 'undefined'){
             equation += parseFloat(value);
         }
         return this;
-    }
+    };
 
     this.multiply = function (value) {
-        equation += '*'
+        equation += '*';
         if(typeof value !== 'undefined'){
             equation += parseFloat(value);
         }
         return this;
-    }
+    };
 
     this.divide = function (value) {
-        equation += '/'
+        equation += '/';
         if(typeof value !== 'undefined'){
             if (value == 0) {
                 throw "Division par zéro!!!";
@@ -74,30 +74,30 @@ var Calculator = function () {
             equation += parseFloat(value);
         }
         return this;
-    }
+    };
 
     this.sin = function(value) {
-        equation += 'Math.sin(' + parseFloat(value) + ')'
+        equation += 'Math.sin(' + parseFloat(value) + ')';
         return this;
-    }
+    };
 
     this.cos = function(value) {
-        equation += 'Math.cos(' + parseFloat(value) + ')'
+        equation += 'Math.cos(' + parseFloat(value) + ')';
         return this;
-    }
+    };
 
     this.tan = function() {
-        equation += 'Math.tan(' + parseFloat(value) + ')'
+        equation += 'Math.tan(' + parseFloat(value) + ')';
         return this;
-    }
+    };
 
     this.setMemory = function(memoryValue) {
         memory = memoryValue;
-    }
+    };
 
     this.getMemory = function() {
         return memory;
-    }
+    };
 
     this.factorial = function(value) {
         if(typeof value === 'undefined'){
@@ -106,7 +106,7 @@ var Calculator = function () {
             equation += 'Math.factorial(' + parseFloat(value) + ')'
         }
         return this;
-    }
+    };
 
     this.equals = function () {
         // Il faut être très prudent avec eval !!! Eval pourrait permettre d'injecter du code malicieux et l'exécuter
@@ -115,91 +115,125 @@ var Calculator = function () {
         var equationSolution = eval(equation);
         equation = '';
         return equationSolution;
+    };
+
+
+
+    this.getEquation = function () {
+        return equation;
     }
-}
+};
 
 
 $(document).ready (function(){
     var calculatrice = new Calculator();
 
     $("#C").click(function(){
-       calculatrice.clear;
+       calculatrice.clear();
         $(".affichage").html(0);
     });
 
     $("#0").click(function(){
         calculatrice.value(0);
-
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#1").click(function(){
         calculatrice.value(1);
-
-
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#2").click(function(){
         calculatrice.value(2);
-
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#3").click(function(){
         calculatrice.value(3);
-
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#4").click(function(){
         calculatrice.value(4);
-
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#5").click(function(){
         calculatrice.value(5);
+        $(".affichage").html(calculatrice.getEquation);
 
-    })
+    });
 
     $("#6").click(function(){
         calculatrice.value(6);
+        $(".affichage").html(calculatrice.getEquation);
 
-    })
+    });
 
     $("#7").click(function(){
         calculatrice.value(7);
+        $(".affichage").html(calculatrice.getEquation);
 
-    })
+    });
 
     $("#8").click(function(){
         calculatrice.value(8);
+        $(".affichage").html(calculatrice.getEquation);
 
-    })
+    });
 
     $("#9").click(function(){
         calculatrice.value(9);
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#additionne").click(function(){
         calculatrice.add();
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#soustrait").click(function(){
         calculatrice.subtract();
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#multiplie").click(function(){
         calculatrice.multiply();
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#divise").click(function(){
         calculatrice.divide();
-    })
+        $(".affichage").html(calculatrice.getEquation);
+    });
 
     $("#factorial").click(function(){
-        calculatrice.factorial();
-    })
+        $(".affichage").html(calculatrice.getEquation())
+        $(".affichage").html(calculatrice.factorial());
+    });
+
+
+
+    var memoire = false;
+    $("#m").click(function(){
+        if(memoire){
+            calculatrice.value(calculatrice.getMemory());
+            $(".affichage").html(calculatrice.getEquation);
+            memoire = false;
+        }
+        else{
+            var resultat = calculatrice.equals();
+            calculatrice.setMemory(resultat);
+            $(".affichage").html(resultat);
+            memoire = true;
+        }
+
+    });
 
     $("#egale").click(function(){
         var resultat = calculatrice.equals();
         $(".affichage").html(resultat);
-    })
+        calculatrice.value(resultat);
+    });
 });
 
